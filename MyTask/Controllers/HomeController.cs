1fc68adfd537cf4920794b73d27044b49734aa71
shafiq-khattana <shafiq.chaudhry.cs@live.com>
2023-01-09 18:27:59 +0500

    First release

---
+++ MyTask/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using MyTask.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult SearchPage()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> ResultPage()
+        {
+            string url = @"http://api.mybookingworld.com/api/OneWayTrip?Origin=KHI&Destination=LHE&Departure=2023-01-12&Adults=1&Children=0&Infant=0&ReturnType=json&UserName=intelli&ApiPassword=786786&ClientID=1&DirectOnly=True&ShowAlternateGrid=False";
+
+            HttpClient client = new HttpClient();
+            var resp = await client.GetAsync(url);
+            if(resp.IsSuccessStatusCode)
+            {
+                string json = await resp.Content.ReadAsStringAsync();
+                DataModel mod = JsonConvert.DeserializeObject<DataModel>(json);
+                return View(mod);
+            }
+            return View();
+        }
+    }
+}
---
+++ MyTask/Models/Flight.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTask.Models
+{
+    public class Flight
+    {
+        public string SequenceNumber { get; set; }
+        public string Origin { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public object DepartureDateFull { get; set; }
+        public string Destination { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public object ArrivalDateFull { get; set; }
+        public string ElapsedTime { get; set; }
+        public string TotalElapsedTime { get; set; }
+        public string MarketingAirline_Code { get; set; }
+        public string MarketingGroup { get; set; }
+        public string FlightNumber { get; set; }
+        public string OperatingAirline_Code { get; set; }
+        public string OperatingAirline_FlightNumber { get; set; }
+        public string AirEquipType { get; set; }
+        public string ResBookDesigCode { get; set; }
+        public string BaseFare { get; set; }
+        public string NonRefundableBaseFare { get; set; }
+        public string FareConstruction { get; set; }
+        public string EquivFare { get; set; }
+        public string ServiceFee { get; set; }
+        public string Tax { get; set; }
+        public string TotalFare { get; set; }
+        public string Extras { get; set; }
+        public string TotalWithExtras { get; set; }
+        public string NegotiatedFareCode { get; set; }
+        public string OnTimePerformanceLevel { get; set; }
+        public string StopQuantity { get; set; }
+        public string Stop { get; set; }
+        public string TourOperationFlightID { get; set; }
+        public string InfoSource { get; set; }
+        public string DisclosureAirLine { get; set; }
+        public string eTicketNumber { get; set; }
+        public string TicketType { get; set; }
+        public string Cabin { get; set; }
+        public string Meal { get; set; }
+        public string SeatsRemaining { get; set; }
+        public string BelowMin { get; set; }
+        public string AccountCode { get; set; }
+        public string AvailabilityBreak { get; set; }
+        public string FareRefundable { get; set; }
+        public string AirlineCode { get; set; }
+        public string TotalFareWithOutAnyOfferAndAdditionalCharg { get; set; }
+        public string AdultQuantity { get; set; }
+        public string AdultBaseFare { get; set; }
+        public string AdultEquivFare { get; set; }
+        public string AdultTax { get; set; }
+        public string AdultTotalFare { get; set; }
+        public string AdultBaggageWeight { get; set; }
+        public string AdultBaggagePieces { get; set; }
+        public string AdultExchangeBeforePenalty { get; set; }
+        public string AdultExchangeAfterPenalty { get; set; }
+        public string AdultRefundBeforePenalty { get; set; }
+        public string AdultRefundAfterPenalty { get; set; }
+        public string ChildQuantity { get; set; }
+        public string ChildBaseFare { get; set; }
+        public string ChildEquivFare { get; set; }
+        public string ChildTax { get; set; }
+        public string ChildTotalFare { get; set; }
+        public string ChildBaggageWeight { get; set; }
+        public string ChildBaggagePieces { get; set; }
+        public string ChildExchangeBeforePenalty { get; set; }
+        public string ChildExchangeAfterPenalty { get; set; }
+        public string ChildRefundBeforePenalty { get; set; }
+        public string ChildRefundAfterPenalty { get; set; }
+        public string InfantQuantity { get; set; }
+        public string InfantBaseFare { get; set; }
+        public string InfantEquivFare { get; set; }
+        public string InfantTax { get; set; }
+        public string InfantTotalFare { get; set; }
+        public string InfantBaggageWeight { get; set; }
+        public string InfantBaggagePieces { get; set; }
+        public string InfantExchangeBeforePenalty { get; set; }
+        public string InfantExchangeAfterPenalty { get; set; }
+        public string InfantRefundBeforePenalty { get; set; }
+        public string InfantRefundAfterPenalty { get; set; }
+        public string Status { get; set; }
+        public string CurrencyCode { get; set; }
+        public string FareBasisCode { get; set; }
+        public string RPH { get; set; }
+        public string OriginCode { get; set; }
+        public string DestinationCode { get; set; }
+        public string AirlineCodeAB { get; set; }
+        public string MarketingAirlineCodeAB { get; set; }
+        public string OfferID { get; set; }
+        public string Owner { get; set; }
+        public string ResponseID { get; set; }
+        public string OfferItemID { get; set; }
+        public string ReferenceID { get; set; }
+
+        public TimeSpan TimeElapsedTS()
+        {
+            return TimeSpan.FromMinutes(Convert.ToDouble(ElapsedTime));
+        }
+    }
+
+    public class DataModel
+    {
+        public List<Flight> Flights { get; set; }
+
+    }
+    
+}
